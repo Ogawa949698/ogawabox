2844cf3c32e2cefe05c0e5bd1f15ad057d724a25
Ogawa34 <rst34shimizuku@stu.kanazawa-u.ac.jp>
2023-06-20 03:08:31 +0900

    6

---
+++ meta/dosam.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#get version number
+ver=`grep -c '^' sample.txt`
+echo $ver
+nver=$((ver + 1))
+echo $nver
+
+#write ver num&brank
+echo -n $nver >> sample.txt
+echo -n "   " >> sample.txt
+
+#write date
+#date | echo -n >> sample.txt
+#type date manualy instead
+echo Date?
+read dat
+echo -n $dat >> sample.txt
+
+exit
+

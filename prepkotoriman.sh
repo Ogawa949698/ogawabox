eb9312b6759865392af172e2ec15bd574d57af13
Ogawa34 <rst34shimizuku@stu.kanazawa-u.ac.jp>
2023-06-20 09:13:16 +0900

    kotoriCOMPLETE?

--- prepkotoriman.sh
+++ prepkotoriman.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-git clone https://github.com/
+cd
+cd ogawabox
+git clone https://github.com/ShellShoccar-jpn/kotoriotoko.git
+cd ./BIN
+echo Plz type "./kotoriotoko/BIN/getstarted.sh
+exit
